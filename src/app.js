b2e079345a516e871c8c4f460d6ea75e373666fd
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-03-09 21:04:36 -0300

    email

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config();
 const router = require('./routes');
+
 // Esta es nuestra aplicación
 const app = express();
-const errorHandler = require('./utils/errorHandler')
 
 // Middlewares 
 app.use(express.json());
@@ -13,14 +13,7 @@ app.use(helmet({
     crossOriginResourcePolicy: false,
 }));
 app.use(cors());
-// routes
-app.get('/', (req, res) =>{
-    return res.send('Welcome to node')
-});
 
 app.use('/api/v1', router);
-
-// middlewares desdpues de las rutas
-app.use(errorHandler)
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,35 @@
 const express = require('express');
-
 const router = express.Router();
-
+const sendEmail = require('../utils/sendEmail');
 // colocar las rutas aquí
-// router.use("/ ", userRouter)
+router.get('/', (req, res) => {
+    res.send("Welcome to express");
+});
+router.post('/emails/sayhi', async ( req, res ) => {
+    await sendEmail({
+        to:"gonzagargantini20@gmail.com",
+        subject:"Porbando Email",
+        html:`
+            <h1 style="color:red">Hola! :D</h1>
+            <p>portafolio html</p>
+        `
+    })
+    return res.json({message: "Email sent"});
+})
+router.post('/emails/contact', async( req, res) =>{
+    const { name, email, phone, message } = req.body;
+    await sendEmail({
+        to:"gonzagargantini20@gmail.com",
+        subject:"Email del Portafolio",
+        html:`<h1>${name}, te escribio desde tu portafolio!</h1>
+        <p>${message}</p>
+        <ul>
+        <li><b>Email</b>${email}</li>
+        <li><b>Telefono</b>${phone}</li>
+        </ul>`
+        
+    })
+    return res.json({message: "Email sent successfully"});
+})
 
 module.exports = router;
